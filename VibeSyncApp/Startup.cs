2f6a82546d98bc69c5e026077e1a805b1a800722
riteshjoshi0410 <ritesh@daffodilsw.com>
2023-07-28 02:25:37 +0530

    Swagger implementation

--- VibeSyncApp/Startup.cs
+++ VibeSyncApp/Startup.cs
@@ -1,12 +1,11 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using VibeSync.DAL.Models;
 using VibeSync.DAL.Models.DAL;
 
@@ -43,6 +42,10 @@ namespace VibeSyncApp
             {
                 configuration.RootPath = "ClientApp/build";
             });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Vibe Sync", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +54,11 @@ namespace VibeSyncApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vibe Sync");
+                });
             }
             else
             {

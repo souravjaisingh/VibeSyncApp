72ff9337a110275607f76e9e6fce5b89ec8a7c5e
Nandini <nandinijaisingh03@gmail.com>
2023-10-14 22:40:57 +0530

    Logo + CSS issues

--- VibeSyncApp/ClientApp/src/components/HeroSection.js
+++ VibeSyncApp/ClientApp/src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      {/* <video src='/videos/video-2.mp4' autoPlay loop muted /> */}
       <h1>Just tell us the song, we'll play</h1>
       <p>What are you waiting for?</p>
     </div>
--- VibeSyncApp/ClientApp/src/components/Navbar.js
+++ VibeSyncApp/ClientApp/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './Navbar.css';
+import vibeSyncLogo from '../Resources/VB_Logo_2.png'; // Import your logo image
 
 function Navbar() {
   const location = useLocation();
@@ -49,74 +50,22 @@ function Navbar() {
   return (
     <>
       <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-  <Link to={linkDecider} className='navbar-logo'>
-    VibeSync 
-    <i className='fab fa-typo3' />
-  </Link>
+        <a href={linkDecider} className='navbar-logo'>
+          <img className='app-logo' src={vibeSyncLogo} alt="VibeSync Logo" />{/* Display your logo image */}
+        </a>
 
-  <div className="ml-auto"> {/* Use ml-auto to push the buttons to the right */}
-    {(location.pathname === '/userhome' 
-      || location.pathname.startsWith('/SongSearch') 
-      || location.pathname.startsWith('/paymentIndex')) && (
-      <button className='btnNav btn--outlineNav btn-sm' onClick={(e) => handleRequestsClick()}>Your Requests</button>
-    )}
+        <div className="ml-auto"> {/* Use ml-auto to push the buttons to the right */}
+          {(location.pathname === '/userhome' 
+            || location.pathname.startsWith('/SongSearch') 
+            || location.pathname.startsWith('/paymentIndex')) && (
+            <button className='btnNav btn--outlineNav btn-sm' onClick={(e) => handleRequestsClick()}>Your Requests</button>
+          )}
 
-    {localStorage.getItem('userId') != null && (
-      <button className='btnNav btn--outlineNav  btn-sm' style={{ marginLeft: '5px' }} onClick={(e) => handleLogoutClick()}>Logout</button>
-    )}
-  </div>
-</nav>
-
-
-
-      {/* <nav className='navbar'>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            Request your song 
-            <i class='fab fa-typo3' />
-          </Link>
-          {/* <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div> */}
-          {/* <ul className={click ? 'nav-menu active' : 'nav-menu'}> */}
-            {/* <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Home
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/services'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Services
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/products'
-                className='nav-links'
-                onClick={closeMobileMenu}
-              >
-                Products
-              </Link>
-            </li> */}
-
-            {/* <li> 
-            
-              <Link
-                to='/sign-up'
-                className='nav-links-mobile'
-                onClick={closeMobileMenu}
-              >
-                Sign Up
-              </Link>
-            {/* </li> */}
-          {/* </ul> *
-          {button && <Button buttonStyle='btn--outline navbar-right'>SIGN UP</Button>}
+          {localStorage.getItem('userId') != null && (
+            <button className='btnNav btn--outlineNav  btn-sm' style={{ marginLeft: '5px' }} onClick={(e) => handleLogoutClick()}>Logout</button>
+          )}
         </div>
-      </nav> */}
+      </nav>
     </>
   );
 }

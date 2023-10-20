0c767d434b709a24e2cd4efffd4f5e6a3fe8ca65
Nandini <nandinijaisingh03@gmail.com>
2023-10-20 13:27:22 +0530

    New connections

--- VibeSyncApp/ClientApp/src/components/Constants.js
+++ VibeSyncApp/ClientApp/src/components/Constants.js
@@ -1,2 +1,2 @@
-export const baseUri = 'https://vibesyncappservice.azurewebsites.net/';
+export const baseUri = 'https://vibesyncapp.azurewebsites.net/';
 export const RazorPayAppId = 'rzp_test_De36fMcorSCmnJ';

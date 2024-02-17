ceb49276d1d6eba8cbd89340ddcac986310bbc00
Nandini <nandinijaisingh03@gmail.com>
2024-02-17 21:32:10 +0530

    Keys

--- VibeSyncApp/ClientApp/src/components/Constants.js
+++ VibeSyncApp/ClientApp/src/components/Constants.js
@@ -1,4 +1,4 @@
 export const baseUri = 'https://vibesyncc.azurewebsites.net/';
-export const RazorPayAppId = 'rzp_live_Lijs0juaFdyE9C';
+export const RazorPayAppId = 'rzp_live_GSCG7iEzR5BhtT';
 export const Live = 'Live';
 export const LiveButNotAcceptingRequests = 'Live-NA';

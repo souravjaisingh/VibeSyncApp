205f7e9f049050f1a287d501ccdc7f23e1460aa0
souravjaisingh <41680168+souravjaisingh@users.noreply.github.com>
2024-03-07 16:36:47 +0530

    Update ContactUs.js

--- VibeSyncApp/ClientApp/src/components/ContactUs.js
+++ VibeSyncApp/ClientApp/src/components/ContactUs.js
@@ -9,7 +9,7 @@ function ContactUs() {
             <p>If you have any questions, suggestions, or need assistance, please don't hesitate to contact us. You can reach us via email, phone, or by mail at the following details:</p>
 
             <p>Email: <a href="mailto:vibesyncdj@gmail.com">vibesyncdj@gmail.com</a></p>
-            <p>Phone: <a href="tel:+1234567890">+91 9896403333</a></p>
+            <p>Phone: <a href="tel:+1234567890">+91 9896403333 (Girik Jain)</a></p>
             <p>Address: #1429/6, Ganesh Bhawan,, Near Old Post Office, Ambala, Haryana, India 134003</p>
 
             <p>We value your feedback and look forward to hearing from you!</p>

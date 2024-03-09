c4f29381cf576079ecab17c82fbbef05f876e075
souravjaisingh <41680168+souravjaisingh@users.noreply.github.com>
2024-03-09 14:18:50 +0530

    Update Constants.js

--- VibeSyncApp/ClientApp/src/components/Constants.js
+++ VibeSyncApp/ClientApp/src/components/Constants.js
@@ -2,3 +2,4 @@ export const baseUri = 'https://vibesyncdj.azurewebsites.net/';
 export const RazorPayAppId = 'rzp_live_GSCG7iEzR5BhtT';
 export const Live = 'Live';
 export const LiveButNotAcceptingRequests = 'Live-NA';
+export const PaidZeroUsingPromocode = 'PaidZeroUsingPromo';

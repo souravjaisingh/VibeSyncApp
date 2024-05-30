fe7616386db93bb826fd7b19f8d39fe5f68340c8
bhavikmittal <bhavikmitt2@gmail.com>
2024-05-30 22:21:03 +0530

    Added image

--- VibeSyncModels/Request_ResponseModels/GetPlaylistTrackModel.cs
+++ VibeSyncModels/Request_ResponseModels/GetPlaylistTrackModel.cs
@@ -9,6 +9,7 @@ namespace VibeSyncModels.Request_ResponseModels
     }
     public class Track
     {
+        public Album Album { get; set; }
         public List<Artist> artists { get; set; }
         public string id { get; set; }
         public string name { get; set; }

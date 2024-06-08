4f0ec060e23e048fe34660af5fd3e26c7e83f5ee
bhavikmittal <bhavikmitt2@gmail.com>
2024-06-08 15:38:07 +0530

    added library

--- VibeSyncApp/Startup.cs
+++ VibeSyncApp/Startup.cs
@@ -80,8 +80,9 @@ namespace VibeSyncApp
             //services.AddScoped<WebSocketHandler>();
 
             services.AddSingleton<HttpClient>();
-            /*var context = new CustomAssemblyLoadContext();
-            context.LoadUnmanagedLibrary(Path.Combine(Directory.GetCurrentDirectory(), "libwkhtmltox"));*/
+            var context = new CustomAssemblyLoadContext();
+            context.LoadUnmanagedLibrary(Path.Combine(Directory.GetCurrentDirectory(), "libwkhtmltox.dll"));
+            context.LoadUnmanagedLibrary(Path.Combine(Directory.GetCurrentDirectory(), "libwkhtmltox.so"));
             services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
 
             // In production, the React files will be served from this directory

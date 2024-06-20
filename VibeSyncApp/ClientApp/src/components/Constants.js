4a12def3501894381437fcc7402def8787d4ddf2
Nandini <nandinijaisingh03@gmail.com>
2024-06-20 17:30:44 +0530

    constants

--- VibeSyncApp/ClientApp/src/components/Constants.js
+++ VibeSyncApp/ClientApp/src/components/Constants.js
@@ -1,6 +1,6 @@
 export const baseUriAzure = 'https://vibesyncdj.azurewebsites.net/';
 export const baseUriVibeSync = 'https://vibesync.in/';
-export const RazorPayAppId = 'rzp_test_Cj4ofnyDnXq2nN';
+export const RazorPayAppId = 'rzp_live_GSCG7iEzR5BhtT';
 export const Live = 'Live';
 export const LiveButNotAcceptingRequests = 'Live-NA';
 export const PaidZeroUsingPromocode = 'PaidZeroUsingPromo';

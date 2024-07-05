cb18c98d8ff400ce866990ceca8ed93e3db8cc78
kritii07 <kritiantwal.7@gmail.com>
2024-07-05 15:15:38 +0530

    Write a service in React to consume Reviews API

--- VibeSyncApp/ClientApp/src/components/services/DjService.js
+++ VibeSyncApp/ClientApp/src/components/services/DjService.js
@@ -9,3 +9,8 @@ export async function UpdateDjDetails(data) {
 export async function GetDjProfile(id) {
     return handleAPIRequest(`Dj/GetDjProfile?UserId=${id}`, 'GET');
 }
+
+// New function to fetch reviews
+export async function GetReviews() {
+    return handleAPIRequest('Dj/GetReviews', 'GET');
+}

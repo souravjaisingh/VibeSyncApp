e1fdb85b875d4d92b39725a40abf5a15b281cd75
bhavikmittal <46300907+bhavikmittal@users.noreply.github.com>
2024-07-17 17:45:04 +0530

    Update PaymentIndex.js

--- VibeSyncApp/ClientApp/src/components/PaymentIndex.js
+++ VibeSyncApp/ClientApp/src/components/PaymentIndex.js
@@ -149,7 +149,7 @@ function PaymentIndex() {
 
         // Check if mic announcement message is empty
         if (isSpecialAnnouncement && !micAnnouncementMessage) {
-            setLocalError('Please Give a message for announcement!');
+            setLocalError('Please give a message for announcement!');
             console.log("Inside this function")
             return; // Stop execution if message is not provided
         }

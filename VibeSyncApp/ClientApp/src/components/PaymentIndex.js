afb3d6bdc5d0a83ee6ca650ee12fba140ce95952
Kk1284 <97325776+Kk1284@users.noreply.github.com>
2024-07-19 18:28:27 +0530

    updated ui changes

--- VibeSyncApp/ClientApp/src/components/PaymentIndex.js
+++ VibeSyncApp/ClientApp/src/components/PaymentIndex.js
@@ -374,6 +374,9 @@ function PaymentIndex() {
                                     if (localError) setLocalError(''); // Clear error message on typing
                                 }} // Update the mic announcement message
                                 />
+                                <div className='subheading-payment'>
+                                    <img src="images/disclaimerIcon.png" className='disclaimer-icon' />
+                                    Played within 30 mins or refund </div>
                                    {localError && <p style={{ color: 'red', fontWeight: 'bold' ,textAlign : 'center' }}>{localError}</p>}
                             </>
                         )}
@@ -400,6 +403,7 @@ function PaymentIndex() {
                                         <input type="file" id="file-upload" />
                                     </div>
                                 </div>
+
                             </>
                         )}
                     </div>
@@ -451,7 +455,7 @@ function PaymentIndex() {
                                 Choose the Tip
                             </div>
                             <div className='tip-amount-input-btns'>
-                                <div onClick={() => { setAmount(amount - 10) }} className='decrease-tip-button'>-</div>
+                                <div onClick={() => { if (amount > 1) { setAmount(amount - 10); } }} className='decrease-tip-button'>-</div>
                                 <input
                                     className='amount-inputfield'
                                     type="number"
@@ -541,7 +545,7 @@ function PaymentIndex() {
                 </div>
 
                 <div className='refund-info-footer'>
-                    <p>~ Should the DJ decline your request, a refund will be issued to your original payment method.</p>
+                    <p>~ If the DJ decline your request, a refund will be issued to your original payment method.</p>
                     <p>~ If DJ accepts the request and doesn't play your song within 30 mins, you'll be issued a full refund.</p>
                 </div>
                 {/* Render the success message if showSuccessMessage is true */}

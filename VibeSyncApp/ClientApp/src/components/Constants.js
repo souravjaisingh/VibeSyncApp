44337970b11a6a1f7b2ca02420f688e7752626aa
Siddharth Amlavad <47478852+siddharth05092004@users.noreply.github.com>
2024-07-31 14:23:51 +0530

    Update Constants.js

--- VibeSyncApp/ClientApp/src/components/Constants.js
+++ VibeSyncApp/ClientApp/src/components/Constants.js
@@ -1,5 +1,5 @@
 export const baseUriAzure = 'https://vibesyncdj.azurewebsites.net/';
-export const baseUriVibeSync = 'https://localhost:5001/';
+export const baseUriVibeSync = 'https://vibesync.in/';
 export const RazorPayAppId = 'rzp_live_GSCG7iEzR5BhtT';
 export const Live = 'Live';
 export const LiveButNotAcceptingRequests = 'Live-NA';

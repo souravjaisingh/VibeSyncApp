1e4795c39d8adf798c3e2c0c34c9ea4589acaab7
Nandini <nandinijaisingh03@gmail.com>
2024-08-05 16:43:36 +0530

    Filtered songs.

--- VibeSync.DAL/Handler/SongsHandler.cs
+++ VibeSync.DAL/Handler/SongsHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -91,6 +92,14 @@ namespace VibeSync.DAL.Handler
         /// Initializes a new instance of the <see cref="SongsHandler"/> class.
         /// </summary>
         /// <param name="client">The client.</param>
+
+        #region
+        //Songs to be filtered out
+        public string[] HaryanviSongs = { "52 GAJ KA DAMAN", "Chatak Matak", "COCO COLA", "Sandal", "Bahu Kale Ki", "Moto", "Left Right", " JUG JUG JEEVE", "Laad Piya Ke", "LALA LORI", "Father Saab", "Teri Aakhya Ka Yo Kajal", "Razzi Bolja", "4G Ka Jamana", "Gurjar Ka Kharcha", "Gypsy", " LOOT LIYA", "Ghunghat Bain", "Badli Badli Laage", "English Medium", "Mulakaat", " Teri Lat Lag Jagi", "UNCHI HAVELI", " Mera Chand", " Yaar Ki Shaadi", "SOLID BODY", "Bholenath", "4G Ka Jamana", "Desi Desi Na Bolya Kar Chori Re", "Mithi Boli","Matak Chalungi", " Ghungroo Toot Jayega", "Halka Dupatta Tera Muh Dikhe", "Lilo Chaman", "Ghagra", "Thada Bhartar", "Jalebi Juda", " GodFather", " Middle Class", "Goli Chal Javegi", "Nazar", " TORA", "Bawli Tared", "Chhutti", "Pistal Te Mehnga Lehnga", "Laada Ka Lada", "Bhagatt Aadmi Tha", "Gunehgar", "Mouj Jamane Main" };
+        public string[] BhojpuriSongs = { "Palang Sagwan Ke", "Maroon Color Sadiya", "Kamariya Dole", "RajaJi Ke Dilwa", "Chhalakata Hamro Jawaniya", "Bas Kar Pagli", "Hook Raja Ji", "Aam Ke Swad", "Mere Marad Mahoday Ji", "Hari Hari Odhani", "Heroine", "Sadiya Bulukiya", "Laal Ghaghra", "Shila Hau Ka", "Baarish Ban Jaana", "Saree Se Tadi", "Laundiya London Se Layenge", "Pagal Banaibu Kare Patar Ki", "Saj Ke Sawar Ke", "Apne Lover Ko Dhokha Do", "Kalkatiya Raja", "Raate Diya Buta Ke Piya Kya Kya Kiya", "Nimbu Kharbuja Bhail 2", "Panche Ke Nache Aiha", "Heek Bhar Nihare Da", "Ae Raja Hamke Banaras Ghumai Da", "Leke Sinhorwa Me", "Dhani Ho Sab Dhan", "Piya Ji Ke Muski", "Chhuye Ke Chhod Di", "Bewfa Tune Tune Pyar Me Badnam Kar Dala", "Tikuliya Ae Raja", "Pyar Mein Naikhi Gori Hum Bewafa", "Aego Baat Batai", "Godi Me Leke", "Paro", "Nathuniya", "Zindagi", "Nadiya Bich Naiya Dole", "Dugo Rakhale Bani", "Sutaal Tani Kora Mein", "Raja Ji", "Kamariya Lollypop Lagelu", "Bewafa Pyar", "Hariyarki Odhaniya", "Love Kala Sab Hoi", "Piyar Farak Wali", "Raja Ji Khoon Kaida", "Dewara Dhodhi Chatana Ba", "Le Le Aayi Coca Cola" };
+        public string[] MusicKeywords = { "Unplugged", "Mashup", "Remix", "Acoustic", "Cover", "Live", "Instrumental", "Original", "Demo", "Extended", "Edit", "Reissue", "Single", "Compilation", "Demo", "Acapella", "Medley", "Tribute", "Version", "Rework", "Bootleg", "Bootleg", "Dub", "Extended Mix", "Radio Edit", "Club Mix", "Remaster", "Rerelease", "Demo", "EP", "LP", "Full-Length", "Track", "Cut", "Score", "Soundtrack", "Jingle", "Theme", "Cover Version", "Interlude", "Outro", "Intro", "Pre-Release", "Preview", "Split", "Virtual", "Live Session", "Mixtape", "Compilation Album", "Greatest Hits", "B-Side", "Alternative" };
+        #endregion
+
         public SongsHandler(HttpClient client, ISongQueryRepository songQueryRepository, IMapper mapper, ISongCommandRepository songCommandRepository, IConfiguration configuration)
         {
             _httpClient = client;
@@ -130,7 +139,10 @@ namespace VibeSync.DAL.Handler
                     var items = jsonObject["data"]["results"].ToString();
                     songDetails = JsonConvert.DeserializeObject<List<SongDetails>>(items);
                     var language = new List<string> { "english", "hindi", "punjabi" };
-                    songDetails = songDetails.Where(x => language.Contains(x.Language) && x.PlayCount != null && x.PlayCount > 50000).ToList();
+                    songDetails = songDetails.Where(x => language.Contains(x.Language) && x.PlayCount != null && x.PlayCount > 50000)
+                        .Where(x => !MusicKeywords.Any(keyword => x.Name.Contains(keyword, StringComparison.OrdinalIgnoreCase)))
+                        .Where(x => !HaryanviSongs.Contains(x.Name, StringComparer.OrdinalIgnoreCase))
+                        .Where(x => !BhojpuriSongs.Contains(x.Name, StringComparer.OrdinalIgnoreCase)).ToList();
                     break; // Break out of the loop if successful
                 }
                 catch (HttpRequestException ex) when (ex.StatusCode != System.Net.HttpStatusCode.OK && ex.StatusCode != System.Net.HttpStatusCode.NoContent)

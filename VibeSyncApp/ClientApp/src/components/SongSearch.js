6ec26b8b08f736a53c030b3926b22830a4abf4e0
Nandini <nandinijaisingh03@gmail.com>
2024-08-25 16:44:34 +0530

    Error page, disclaimer fixed, telephone

--- VibeSyncApp/ClientApp/src/components/SongSearch.js
+++ VibeSyncApp/ClientApp/src/components/SongSearch.js
@@ -474,13 +474,16 @@ function SongSearch() {
                             <img style={{ width: '15px' }} src="/images/disclaimerIcon.png" />
                             <b><div>{eventData.eventDescription}</div></b>
                         </span>
-                        <span className='time-disclaimer'>
-                        <svg width="20" height="20" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                          <path d="M12 2L2 22h20L12 2z" fill="none" stroke="black" stroke-width="2" />
-                          <path d="M12 10v4M12 18h.08" stroke="black" stroke-width="2" />
-                        </svg>
-                            <b><div>&nbsp;{timeDisclaimer}</div></b>
-                        </span>
+                        {timeDisclaimer && timeDisclaimer.trim() !== '' && (
+                            <span className='time-disclaimer'>
+                                <svg width="20" height="20" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                    <path d="M12 2L2 22h20L12 2z" fill="none" stroke="black" stroke-width="2" />
+                                    <path d="M12 10v4M12 18h.08" stroke="black" stroke-width="2" />
+                                </svg>
+                                <b><div>&nbsp;{timeDisclaimer}</div></b>
+                            </span>
+                        )}
+
                     </div>
 
                 )}
